311f4e0d53105731d471f2b7c4e38d93449b2ee1
unknown <jessespeks@hotmail.com>
2022-06-27 20:14:03 -0400

    first commit

---
+++ asynch.js
@@ -0,0 +1,51 @@
+function printEvery5Sec(){
+    setTimeout(print, 5000)
+}
+
+function print() {
+    console.log("print")
+}
+
+printEvery5Sec()
+
+// Review from activity, both do the same thing. The second more efficiently than the other
+
+//character.walkEast()
+//setTimeout(character.stop, 500)
+//setTimeout(character.walkNorth, 500)
+//setTimeout(character.stop, 1000)
+//setTimeout(character.walkEast, 1000)
+//setTimeout(character.stop, 1500)
+
+// setTimeout(() => {
+//   character.walkEast();
+//   setTimeout(() => {
+//     character.walkNorth();
+//     setTimeout(() => {
+//       character.walkEast();
+//       setTimeout(() => {
+//         character.stop()
+//       }, 500)
+//     }, 500)
+//   }, 500)
+// }, 500)
+
+// Looping the character to walk in a circle
+
+// function startLoop(){
+//     // Your code here
+//   character.walkEast()
+//   setTimeout(() => {
+//     character.walkNorth()
+//     setTimeout(() => {
+//       character.walkWest()
+//       setTimeout(() => {
+//         character.walkSouth()
+//         setTimeout(() => {
+//           startLoop()
+//         }, 500)
+//       }, 1000)
+//     }, 500)
+//   }, 1000)
+// }
+
